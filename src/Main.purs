f445cd43570cf832ca3e3e35502001a699ae2bce
Hakim Cassimally <hakim.cassimally@gmail.com>
2020-07-10 10:09:56 +0100

    Sketch subprocess and basic parser
    Playing with ChildProcess (via Sunde) and StringParser.

--- src/Main.purs
+++ src/Main.purs
@@ -1,10 +1,54 @@
 module Main where
 
 import Prelude
+import Node.ChildProcess as CP
+import Sunde as S
+import Data.Maybe (Maybe(..))
+
+import Effect.Aff (Aff, launchAff_)
+import Effect.Class.Console as Console
 
 import Effect (Effect)
 import Effect.Console (log)
 
+-- import Text.Parsing.StringParser.Combinators (many)
+import Data.Array (many)
+import Data.String.CodeUnits (fromCharArray)
+import Control.Alt ((<|>))
+import Text.Parsing.StringParser (Parser, runParser)
+import Text.Parsing.StringParser.CodeUnits (string, eof, anyChar)
+
+import Data.Generic.Rep
+import Data.Generic.Rep.Show
+
 main :: Effect Unit
-main = do
-  log "🍝"
+main = launchAff_ do
+          result <- S.spawn { args: [], cmd: "ls", stdin: Nothing }  CP.defaultSpawnOptions
+          Console.log result.stdout
+
+data Line = Plus String
+          | Minus String
+          | NewLine
+derive instance genericLine :: Generic Line _
+instance showLine :: Show Line where
+  show = genericShow
+
+line :: Parser Line
+line = plusOrMinus "+" Plus
+       <|> plusOrMinus "-" Minus
+       <|> newline
+
+plusOrMinus :: String -> (String -> Line) -> Parser Line
+plusOrMinus s a = do
+  _ <- string s
+  l <- many anyChar
+  _ <- eof
+  pure $ a (fromCharArray l)
+
+newline :: Parser Line
+newline = do
+  _ <- string "~"
+  _ <- eof
+  pure $ NewLine
+
+parseLine = runParser line
