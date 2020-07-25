fef95df9f57d70a71011c08d2eca0f588a4948a4
Hakim Cassimally <hakim.cassimally@gmail.com>
2020-07-25 23:33:26 +0100

    Document regex
    The multiline flag isn't needed for this regex, but might be for
    user-supplied ones that include e.g. `^` or `$`

--- src/Source.purs
+++ src/Source.purs
@@ -8,7 +8,7 @@ import Data.Traversable (scanl)
 
 import Data.String (length)
 import Data.String.Regex (Regex, regex, split)
-import Data.String.Regex.Flags (global, multiline)
+import Data.String.Regex.Flags (multiline, noFlags)
 
 import Effect (Effect)
 import Effect.Console (logShow)
@@ -17,7 +17,7 @@ example :: String
 example = "module\n\nimport\nimport\nimport\n\ndata Foo =\n   Foo a\n   |Baz b\n\nfoo :: Foo -> Int\nfoo = identity"
 
 compile :: String -> Either String Regex
-compile re = regex re (global <> multiline)
+compile re = regex re multiline
 
 regexIndices :: String -> String -> Either String (List Int)
 regexIndices re str = do
@@ -25,6 +25,19 @@ regexIndices re str = do
     strings <- pure $ fromFoldable $ split rx str
     pure $ 0 : scanl (\b a -> b + length a) 0 strings
 
+-- | Hunks start with a line at Column 0 that follows an empty line.
+-- |
+-- | This supports:
+-- |
+-- |  * PureScript/Haskell/Python like indentation rules
+-- |    (including multiple contiguous lines at col 0 at start, 
+-- |    e.g. type signature)
+-- |  * Blocks of imports (col 0) grouped together and separated by
+-- |    an empty line
+-- |  * Java/C/Perl/Ruby style with an ending `}` or `end` token, as
+-- |    long as it isn't separated by an empty line
+-- |  * empty lines in the middle of a function (as long as they're followed by
+-- |    an indented line)
 indices :: String -> Either String (List Int)
 indices = regexIndices "(?<=\\n\\n)(?=\\S)"
 
