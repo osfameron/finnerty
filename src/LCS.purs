d13dcd5a450d4f32fd068ffe1479c488a43c1af9
Hakim Cassimally <hakim.cassimally@gmail.com>
2020-08-15 03:37:57 +0100

    LCS algorithm
    Not memoized, so unoptimized, but ok for demonstration

--- src/LCS.purs
+++ src/LCS.purs
@@ -1,10 +1,23 @@
 module LCS where
 
 import Prelude
-import Data.List
+import Data.List (List(..), (:))
+import Data.List as L
+import Data.Array as A
 
-lcs :: forall a. Eq a => List a -> List a -> List a
-lcs Nil _ = Nil
-lcs _ Nil = Nil
-lcs (a:_) (b:_) | a == b = a : Nil
-lcs _ _ = Nil
+lcs :: forall a. Eq a => Array a -> Array a -> Array a
+lcs a b = A.fromFoldable $ lcsa (L.fromFoldable a) (L.fromFoldable b)
+
+lcsa :: forall a. Eq a => List a -> List a -> List a
+lcsa Nil _ = Nil
+lcsa _ Nil = Nil
+lcsa (a:as) (b:bs) | a == b = a : lcsa as bs
+lcsa as@(_:as') bs@(_:bs') =
+    longest
+        (lcsa as bs')
+        (lcsa as' bs)
+
+longest :: forall a. List a -> List a -> List a
+longest a b = if L.length a > L.length b
+              then a
+              else b
--- test/LCS.purs
+++ test/LCS.purs
@@ -12,15 +12,32 @@ import Data.List
 main :: Effect Unit
 main = runTest do
   suite "LCS" do
-    let empty = Nil :: List Int
-    let one = 1 : Nil
+    let empty = [] :: Array Int
 
     test "empty" do
         Assert.equal empty (lcs empty empty)
     test "single" do
-        Assert.equal one (lcs one one)
+        Assert.equal [1] (lcs [1] [1])
     test "no match" do
-        Assert.equal empty (lcs empty one)
-        Assert.equal empty (lcs one empty)
+        Assert.equal empty (lcs empty [1])
+        Assert.equal empty (lcs [1] empty)
+    test "one match at start" do
+        Assert.equal [1] (lcs [1] [1,2])
+        Assert.equal [1] (lcs [1,2] [1])
+        Assert.equal [1] (lcs [1,2] [1,3])
+        Assert.equal [1] (lcs [1,3] [1,2])
+    test "two match at start" do
+        Assert.equal [1,2] (lcs [1,2] [1,2,3])
+        Assert.equal [1,2] (lcs [1,2,3] [1,2])
+        Assert.equal [1,2] (lcs [1,2,3] [1,2])
+    test "match at middle" do
+        Assert.equal [2] (lcs [1,2,4] [2,3])
+        Assert.equal [2,3] (lcs [1,2,3,4] [2,3])
+        Assert.equal [2,3] (lcs [1,2,3,4] [0,2,3,5])
+    test "match interspersed" do
+        Assert.equal [2,3,5] (lcs [2,3,4,5] [1,2,3,3,5,6])
+
+
+
 
 
